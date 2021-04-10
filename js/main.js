6df94a727978ed50e8ea3e61de74c6e2062e6a44
LAG17-xd <78927626+LAG17-xd@users.noreply.github.com>
2021-04-09 19:15:52 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,14 +12,14 @@ $(document).ready(function(){
 	});
 	$('.btn-exit-system').on('click', function(){
 		swal({
-		  	title: 'Are you sure?',
-		  	text: "The current session will be closed",
+		  	title: '¿Estas seguro?',
+		  	text: "Se cerrará la sesión actual",
 		  	type: 'warning',
 		  	showCancelButton: true,
 		  	confirmButtonColor: '#03A9F4',
 		  	cancelButtonColor: '#F44336',
-		  	confirmButtonText: '<i class="zmdi zmdi-run"></i> Yes, Exit!',
-		  	cancelButtonText: '<i class="zmdi zmdi-close-circle"></i> No, Cancel!'
+		  	confirmButtonText: '<i class="zmdi zmdi-run"></i> Si, Salir!',
+		  	cancelButtonText: '<i class="zmdi zmdi-close-circle"></i> No, Cancelar!'
 		}).then(function () {
 			window.location.href="index.html";
 		});
